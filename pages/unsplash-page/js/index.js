99a1d6f9ec779c8e6c4ae84b7ea889487c16fd90
MaisenRv <dsmr19961@gmail.com>
2023-03-19 01:37:20 -0500

    Actualizacion de estilos y a la inforamcion inicial del fetch

--- pages/unsplash-page/js/index.js
+++ pages/unsplash-page/js/index.js
@@ -3,16 +3,46 @@ const sectionImages = document.getElementById("fetch-api")
 const printInformation = (data) =>{
     data.then(result =>{
         for(photoItem of result){
-            const {urls} = photoItem
+            console.log(photoItem);
+            const {urls, user, created_at,likes, alt_description} = photoItem
             const {regular} = urls
 
+            // Create photo element
             const article = document.createElement("article")
             article.classList = "photoItem"
             const img = document.createElement("img")
             img.src = regular
-            // img.style.width = "200px"
-            // img.style.height = "200px"
+
+            // photo Information
+            const infoPhotoItem = document.createElement("div")
+            infoPhotoItem.classList = "photoItem-info"
+
+            const userName = document.createElement("h3")
+            userName.textContent = user.name
+
+            const createdDate = document.createElement("span")
+            createdDate.textContent = `Publicado el ${created_at.substring(0,10)}`
+
+            const description = document.createElement("p")
+            description.textContent = alt_description
+
+            // Likes------------------------------
+            const photoLikes = document.createElement("span")
+            const numberLikes = document.createTextNode(likes)
+            const likeSVG = document.createElement('img')
+            likeSVG.src = "../../../images/thumb-up.svg"
+            likeSVG.classList = "img-like"
+            photoLikes.appendChild(likeSVG)
+            photoLikes.appendChild(numberLikes)
+            // ----------------------------------
+
+            infoPhotoItem.appendChild(userName)
+            infoPhotoItem.appendChild(description)
+            infoPhotoItem.appendChild(photoLikes)
+            infoPhotoItem.appendChild(createdDate)
+
             article.appendChild(img)
+            article.appendChild(infoPhotoItem)
             sectionImages.appendChild(article)
         }
 
